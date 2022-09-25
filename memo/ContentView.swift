a170512ba64e8f5ed24a67d2497c5809d35cbf7b
KantaHasegawa <66783124+KantaHasegawa@users.noreply.github.com>
2022-09-25 21:36:38 +0900

    Initial Commit

---
+++ memo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memo
+//
+//  Created by KantaHasegawa on 2022/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memo/memoApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoApp.swift
+//  memo
+//
+//  Created by KantaHasegawa on 2022/09/25.
+//
+
+import SwiftUI
+
+@main
+struct memoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
