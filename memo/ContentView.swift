bd08b6d851daee6bc30870c3d8d830ebecb3d147
KantaHasegawa <66783124+KantaHasegawa@users.noreply.github.com>
2022-09-26 13:44:36 +0900

    init

--- memo/ContentView.swift
+++ memo/ContentView.swift
@@ -7,10 +7,80 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MemoList: View {
+    @Binding var memos: [String]
+    var Remove: (Int) -> Void
+    var body: some View{
+        VStack {
+            Text("Memo List")
+            List(memos.indices, id: \.self) {index in
+                Memo(memo: memos[index], index: index, Remove: Remove)
+            }
+        }
+    }
+}
+
+struct Memo: View {
+    var memo: String
+    var index: Int
+    var Remove: (Int) -> Void
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            Text("title: \(memo)")
+            Button(action: {Remove(index)}){
+                Text("Remove")
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var text: String = ""
+    @State private var memos: [String] = []
+       
+    var body: some View {
+        VStack {
+            Text("My memo App!!")
+                .padding()
+            Button(action: {CallAPI()}){
+               Text("API Call!!")
+            }
+            TextField("memo title", text: $text)
+            HStack {
+                Button(action: {Add()}){
+                    Text("add")
+                }
+            }
+            MemoList(memos: $memos, Remove: Remove)
+        }
+    }
+    
+    func Add () {
+        if text == "" {return}
+        memos.append(text)
+        text = ""
+        return
+    }
+    
+    func Remove (index: Int) {
+        memos.remove(at: index)
+        return
+    }
+    
+    func CallAPI () {
+        let url = URL(string: "http://localhost:3000/users")!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in  //非同期で通信を行う
+            guard let data = data else { return }
+            do {
+                let object = try JSONSerialization.jsonObject(with: data, options: [])  // DataをJsonに変換
+                print(object)
+            } catch let error {
+                print(error)
+            }
+        }
+        task.resume()
+    
     }
 }
 
